e2242894ac2f502157a5f40982365732f005327b
ajittgosavii <162774684+ajittgosavii@users.noreply.github.com>
2025-06-19 01:54:36 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,14 +158,14 @@ class EnhancedAWSPricingAPI:
         total_cost = 0
     
     # Writer cost
-            writer_pricing = self.get_rds_pricing(region, engine, writer_instance, writer_multi_az)
-            total_cost += writer_pricing['hourly']
+        writer_pricing = self.get_rds_pricing(region, engine, writer_instance, writer_multi_az)
+        total_cost += writer_pricing['hourly']
     
-        for reader_instance, multi_az in reader_instances:
+    for reader_instance, multi_az in reader_instances:
             reader_pricing = self.get_rds_pricing(region, engine, reader_instance, multi_az)
             total_cost += reader_pricing['hourly']
     
-        return total_cost
+    return total_cost
     
         
 def get_rds_pricing(self, region: str, engine: str, instance_class: str, multi_az: bool = False) -> Dict:
